296d9419e2df074c5078cc8a9ca62ae28a4ed20c
mieszko4 <mieszko4@gmail.com>
2017-03-20 17:23:24 +0100

    Send only new IP addresses

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const fs = require('fs');
 const Promise = require('bluebird');
 const Slack = require('slack-node');
 
 const lookup = Promise.promisify(require('dns-lookup'));
 
 const config = require('./config');
+const storage = {}; // in memory storage
 
 slack = new Slack();
 slack.setWebhook(config.slack.webhookUri);
@@ -21,16 +23,39 @@ const sendMessage = (text) => {
   });
 }
 
+const resolveDns = () => {
+  Promise.map(config.domains, domain => lookup(domain))
+  .then(function(addresses) {
+    const mapping = {};
+    addresses.forEach((address, i) => {
+      const domain = config.domains[i];
+      mapping[domain] = address;
+    });
 
-Promise.map(config.domains, domain => lookup(domain))
-.then(function(addresses) {
-  const mapping = {};
-  addresses.forEach((address, i) => {
-    mapping[config.domains[i]] = address;
+    // check if any new
+    const newMapping = {};
+    console.log('ChECKING');
+    Object.keys(mapping).forEach(domain => {
+      const ip = mapping[domain];
+
+      const storedIps = storage[domain] || [];
+
+      if (storedIps.indexOf(ip) === -1) {
+        storedIps.push(ip);
+        newMapping[domain] = ip;
+        storage[domain] = storedIps;
+      }
+    });
+
+    if (Object.keys(newMapping).length > 0) {
+      sendMessage(`*NEW IP ADDRESSES*: \`\`\`${JSON.stringify(mapping, null, 2)}\`\`\``);
+    }
+  }).catch((error) => {
+    const message = (error && error.message) || 'Unknown';
+    sendMessage(`*DNS LOOKUP ERROR*:\n${message}`);
+  }).finally(() => {
+    setTimeout(resolveDns, 5000);
   });
+};
 
-  sendMessage(`*NEW IP ADDRESSES*: \`\`\`${JSON.stringify(mapping, null, 2)}\`\`\``);
-}).catch((error) => {
-  const message = (error && error.message) || 'Unknown';
-  sendMessage(`*DNS LOOKUP ERROR*:\n${message}`);
-});
+resolveDns();
